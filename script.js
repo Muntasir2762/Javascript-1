ac121ff88c039756f254ea40efb6c299427ffb5d
Munotasir2762 <muntasirpranto2762@gmail.com>
2026-01-07 23:41:06 +0600

    Arithmatic operator

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+// let number1 = 790;
+// let number2 = 567;
+
+
+// let sum = number1+number2;
+// console.log('The summation is = '+sum);
+
+
+// let sub = number1-number2;
+// console.log('The subtraction is = '+sub);
+
+// let multiplication = number1*number2;
+// console.log('The multiplication is= '+multiplication);
+
+// let div = number1/number2;
+// console.log('The diviosion is = '+div);
+
+// let remain = number1%number2;
+// console.log('The remainder is = '+remain);
+
+
+let rahimTotalMoney = 50000;
+let eggQty = 500;
+let kitkatQty = 50;
+
+let eggUnitPrice = 10;
+let kitkatUnitPrice = 40;
+
+let totalCost = (eggQty*eggUnitPrice)+(kitkatQty*kitkatUnitPrice);
+
+console.log('Total Money = '+rahimTotalMoney);
+console.log('Total Cost = '+totalCost);
+
+let remainingMoney = rahimTotalMoney-totalCost;
+console.log('The remaining money = '+remainingMoney);
